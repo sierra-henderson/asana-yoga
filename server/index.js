0114123a7bf2e34f1c61a0cff961010988a4ab27
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-12 22:16:44 +0000

    Create new get route for cart

--- server/index.js
+++ server/index.js
@@ -52,6 +52,10 @@ app.get('/api/products/:productId', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/cart', (req, res, next) => {
+  res.json([]);
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
