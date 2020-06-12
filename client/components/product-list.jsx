b05ac7f26b4acf23d58b8a06f5af59c56d326584
Sierra Henderson <henderson.sierra@yahoo.com>
2020-06-12 17:50:34 +0000

    Created Product List component, added state and getProducts method

---
+++ client/components/product-list.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable no-console, no-unused-vars */
+
+import React from 'react';
+import { response } from 'express';
+
+export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(data => {
+        // eslint-disable-next-line no-console
+        console.log(data);
+      });
+  }
+}
