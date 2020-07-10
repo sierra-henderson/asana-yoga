9ad7f2e795832861c0408756a5af797f307f43de
Sierra Henderson <henderson.sierra@yahoo.com>
2020-07-10 23:56:01 +0000

    Adjusted grid for mobile, added media queries

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -3,7 +3,7 @@ import CartSummaryItem from './cart-summary-item';
 
 export default class CartSummary extends React.Component {
   render() {
-    const totalPrice = this.props.products.reduce((acc, cur) => acc + (cur.price / 100), 0);
+    const totalPrice = this.props.products.reduce((acc, cur) => acc + (cur.price / 100), 0).toFixed(2);
     if (this.props.products.length === 0) {
       return (
         <div className="container">
@@ -20,14 +20,14 @@ export default class CartSummary extends React.Component {
         </div>
         <h1 className="font-weight-bold">My Cart</h1>
         <div className="row">
-          <div className="col-8">
+          <div className="col-12 col-lg-8">
             {
               this.props.products.map(product => {
                 return <CartSummaryItem key={product.cartItemId} product={product} />;
               })
             }
           </div>
-          <div className="col-4">
+          <div className="col-12 col-lg-4">
             <div className="card p-5 mt-4 text-center">
               <h3 className="font-weight-bold">Item Total</h3>
               <div className="divider mt-2 mb-3"></div>
