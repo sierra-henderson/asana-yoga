9d85925b8a470769d17d57ad62813f874dd79b18
Sierra Henderson <henderson.sierra@yahoo.com>
2020-08-06 23:45:54 +0000

    Changed database to include categories

--- database/dump.sql
+++ database/dump.sql
@@ -16,18 +16,23 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE ONLY public.products DROP CONSTRAINT "products_categoryId_fkey";
 ALTER TABLE ONLY public.products DROP CONSTRAINT products_pkey;
 ALTER TABLE ONLY public.orders DROP CONSTRAINT orders_pkey;
+ALTER TABLE ONLY public.categories DROP CONSTRAINT categories_pkey;
 ALTER TABLE ONLY public.carts DROP CONSTRAINT carts_pkey;
 ALTER TABLE ONLY public."cartItems" DROP CONSTRAINT "cartItems_pkey";
 ALTER TABLE public.products ALTER COLUMN "productId" DROP DEFAULT;
 ALTER TABLE public.orders ALTER COLUMN "orderId" DROP DEFAULT;
+ALTER TABLE public.categories ALTER COLUMN "categoryId" DROP DEFAULT;
 ALTER TABLE public.carts ALTER COLUMN "cartId" DROP DEFAULT;
 ALTER TABLE public."cartItems" ALTER COLUMN "cartItemId" DROP DEFAULT;
 DROP SEQUENCE public."products_productId_seq";
 DROP TABLE public.products;
 DROP SEQUENCE public."orders_orderId_seq";
 DROP TABLE public.orders;
+DROP SEQUENCE public."categories_categoryId_seq";
+DROP TABLE public.categories;
 DROP SEQUENCE public."carts_cartId_seq";
 DROP TABLE public.carts;
 DROP SEQUENCE public."cartItems_cartItemId_seq";
@@ -129,6 +134,36 @@ ALTER SEQUENCE public."carts_cartId_seq" OWNED BY public.carts."cartId";
 
 
 --
+-- Name: categories; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.categories (
+    "categoryId" integer NOT NULL,
+    name text NOT NULL
+);
+
+
+--
+-- Name: categories_categoryId_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public."categories_categoryId_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: categories_categoryId_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."categories_categoryId_seq" OWNED BY public.categories."categoryId";
+
+
+--
 -- Name: orders; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -168,6 +203,7 @@ ALTER SEQUENCE public."orders_orderId_seq" OWNED BY public.orders."orderId";
 
 CREATE TABLE public.products (
     "productId" integer NOT NULL,
+    "categoryId" integer,
     name text NOT NULL,
     price integer NOT NULL,
     image text NOT NULL,
@@ -211,6 +247,13 @@ ALTER TABLE ONLY public.carts ALTER COLUMN "cartId" SET DEFAULT nextval('public.
 
 
 --
+-- Name: categories categoryId; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.categories ALTER COLUMN "categoryId" SET DEFAULT nextval('public."categories_categoryId_seq"'::regclass);
+
+
+--
 -- Name: orders orderId; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -241,6 +284,17 @@ COPY public.carts ("cartId", "createdAt") FROM stdin;
 
 
 --
+-- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.categories ("categoryId", name) FROM stdin;
+1	Mat Supplies
+2	Meditation
+3	Props
+\.
+
+
+--
 -- Data for Name: orders; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -252,17 +306,17 @@ COPY public.orders ("orderId", "cartId", name, "creditCard", "shippingAddress", 
 -- Data for Name: products; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.products ("productId", name, price, image, "shortDescription", "longDescription") FROM stdin;
-1	Anti-Slip Yoga Mat	5995	/images/yoga-mat.jpg	Made with natural and ethically sourced rubber, this mat is as comfortable as it is environmentally responsible.	Made with natural and ethically sourced rubber, this mat is as comfortable as it is environmentally responsible. Let the calming blue colors of the mat soothe you during your practice. The anti-gripping technology makes sure you feel secure in the most challenging of poses. Also suitable for hot yoga.
-4	Meditation Cushion	4495	/images/meditation-cushion.jpg	For those who want a little extra support while meditating, this meditation cushion has you covered.	For those who want a little extra support while meditating, this meditation cushion has you covered. The plush surface makes sure you are comfortable while still maintaining your posture. Can be used in combination with bolsters, blankets, and blocks for a more personalized meditation experience.
-5	Soothing Eye Pillow	945	/images/eye-pillow.jpeg	Enhanced with lavender sprigs and essential oils, this eye pillow helps guarantee a soothing meditation experience.	This product is inspired by one of the owner’s favorite yoga teachers, who would place homemade eye pillows beside each pupil as they made their way into Shavasana. The buckwheat kernels distribute an even amount of weight over the eyes to further induce relaxation. Enhanced with lavender sprigs and essential oils, this eye pillow helps guarantee a soothing meditation experience.
-2	Cork Yoga Block	2195	/images/cork-block.jpg	The cork material makes this block an environmentally friendly alternative to standard foam blocks without sacrificing quality.	The cork material makes this block an environmentally friendly alternative to standard foam blocks without sacrificing quality. It makes an extremely versatile prop, with three height options that can be used either alone or in combination with other props. Those with less flexibility can also use this block to do easier or restorative variations of poses.
-3	Rectangular Bolster	4545	/images/bolster-2.jpg	Ideal for people with injuries, the elderly, or beginners to ease into the practice of yoga.	Ideal for people with injuries, the elderly, or beginners to ease into the practice of yoga. A perfect prop for restorative work, this bolster can be used to support parts of the body into more relaxing poses. Also can be used in combination with other props or in chest opening exercises.
-6	Brass Singing Bowl	9995	/images/singing-bowl.jpg	Harness the powers of sound healing with this beautiful brass singing bowl.	Harness the powers of sound healing with this beautiful brass singing bowl. Whether you lead a meditation group or want to bring sound into your own practice, this singing bowl brings meditation into another level. Can also be used by itself in the home.
-7	Protective Yoga Bag	2495	/images/yoga-bag.jpg	Keep your mat looking and feeling brand new with our protective yoga bag.	Keep your mat looking and feeling brand new with our protective yoga bag. The cross-body strap makes this bag easy to carry. The durable canvas keeps your mat protected in cramped cars, smelly gym lockers and cross country moves. No matter what is going on in your life, rest easy knowing that your yoga mat is protected.
-8	Energizing Mat Wash	895	/images/mat-wash.jpg	This mat wash is specifically formulated with essential oils to keep your mat clean without any harsh chemicals.	This mat wash is specifically formulated with essential oils to keep your mat clean without any harsh chemicals. The tea tree oil is used as a natural anti-bacterial to keep your mat clean. With tangerine and lemongrass essential oils, its invigorating scent will stay after multiple uses.
-9	Woven Yoga Blanket	2045	/images/blanket.jpg	Made with natural fibers, this yoga blanket is the perfect addition to your meditation practice or yoga practice.	Made with natural fibers, this yoga blanket is the perfect addition to your meditation practice or yoga practice. The thin blanket can either be spread out thin to protect your yoga mat or be folded up thick to cushion your body. It also works well in combination with other props such as bolsters and blocks for a restorative experience.
-10	Yoga Strap	1295	/images/yoga-strap.jpg	This yoga strap will give you the extra boost you need to get deeper into stretches and hold them for a longer period of time.	This yoga strap will give you the extra boost you need to get deeper into stretches and hold them for a longer period of time. The stainless steel buckle allows the strap to be tightened into a loop, allowing you to hold your body in positions without holding onto the strap. Also helpful for beginners that need that extra help in attaining specific poses.
+COPY public.products ("productId", "categoryId", name, price, image, "shortDescription", "longDescription") FROM stdin;
+1	1	Anti-Slip Yoga Mat	5995	/images/yoga-mat.jpg	Made with natural and ethically sourced rubber, this mat is as comfortable as it is environmentally responsible.	Made with natural and ethically sourced rubber, this mat is as comfortable as it is environmentally responsible. Let the calming blue colors of the mat soothe you during your practice. The anti-gripping technology makes sure you feel secure in the most challenging of poses. Also suitable for hot yoga.
+2	3	Cork Yoga Block	2195	/images/cork-block	The cork material makes this block an environmentally friendly alternative to standard foam blocks without sacrificing quality.	The cork material makes this block an environmentally friendly alternative to standard foam blocks without sacrificing quality. It makes an extremely versatile prop, with three height options that can be used either alone or in combination with other props. Those with less flexibility can also use this block to do easier or restorative variations of poses.
+3	3	Rectangular Bolster	4545	/images/bolster-1.jpg	Ideal for people with injuries, the elderly, or beginners to ease into the practice of yoga.	Ideal for people with injuries, the elderly, or beginners to ease into the practice of yoga. A perfect prop for restorative work, this bolster can be used to support parts of the body into more relaxing poses. Also can be used in combination with other props or in chest opening exercises.
+4	2	Meditation Cushion	4495	/images/meditation-cushion.jpg	For those who want a little extra support while meditating, this meditation cushion has you covered.	For those who want a little extra support while meditating, this meditation cushion has you covered. The plush surface makes sure you are comfortable while still maintaining your posture. Can be used in combination with bolsters, blankets, and blocks for a more personalized meditation experience.
+5	2	Soothing Eye Pillow	945	/images/eye-pillow.jpeg	Enhanced with lavender sprigs and essential oils, this eye pillow helps guarantee a soothing meditation experience.	This product is inspired by one of the owner’s favorite yoga teachers, who would place homemade eye pillows beside each pupil as they made their way into Shavasana. The buckwheat kernels distribute an even amount of weight over the eyes to further induce relaxation. Enhanced with lavender sprigs and essential oils, this eye pillow helps guarantee a soothing meditation experience.
+6	2	Brass Singing Bowl	9995	/images/singing-bowl.jpg	Harness the powers of sound healing with this beautiful brass singing bowl.	Harness the powers of sound healing with this beautiful brass singing bowl. Whether you lead a meditation group or want to bring sound into your own practice, this singing bowl brings meditation into another level. Can also be used by itself in the home.
+7	1	Protective Yoga Bag	2495	/images/yoga-bag.jpg	Keep your mat looking and feeling brand new with our protective yoga bag.	Keep your mat looking and feeling brand new with our protective yoga bag. The cross-body strap makes this bag easy to carry. The durable canvas keeps your mat protected in cramped cars, smelly gym lockers and cross country moves. No matter what is going on in your life, rest easy knowing that your yoga mat is protected.
+8	1	Energizing Mat Wash	895	/images/mat-wash.jpg	This mat wash is specifically formulated with essential oils to keep your mat clean without any harsh chemicals.	This mat wash is specifically formulated with essential oils to keep your mat clean without any harsh chemicals. The tea tree oil is used as a natural anti-bacterial to keep your mat clean. With tangerine and lemongrass essential oils, its invigorating scent will stay after multiple uses.
+9	3	Woven Yoga Blanket	2045	/images/blanket.jpg	Made with natural fibers, this yoga blanket is the perfect addition to your meditation practice or yoga practice.	Made with natural fibers, this yoga blanket is the perfect addition to your meditation practice or yoga practice. The thin blanket can either be spread out thin to protect your yoga mat or be folded up thick to cushion your body. It also works well in combination with other props such as bolsters and blocks for a restorative experience.
+10	3	Yoga Strap	1295	/images/yoga-strap.jpg	This yoga strap will give you the extra boost you need to get deeper into stretches and hold them for a longer period of time.	This yoga strap will give you the extra boost you need to get deeper into stretches and hold them for a longer period of time. The stainless steel buckle allows the strap to be tightened into a loop, allowing you to hold your body in positions without holding onto the strap. Also helpful for beginners that need that extra help in attaining specific poses.
 \.
 
 
@@ -270,14 +324,21 @@ COPY public.products ("productId", name, price, image, "shortDescription", "long
 -- Name: cartItems_cartItemId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."cartItems_cartItemId_seq"', 54, true);
+SELECT pg_catalog.setval('public."cartItems_cartItemId_seq"', 55, true);
 
 
 --
 -- Name: carts_cartId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."carts_cartId_seq"', 50, true);
+SELECT pg_catalog.setval('public."carts_cartId_seq"', 51, true);
+
+
+--
+-- Name: categories_categoryId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public."categories_categoryId_seq"', 3, true);
 
 
 --
@@ -311,6 +372,14 @@ ALTER TABLE ONLY public.carts
 
 
 --
+-- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY ("categoryId");
+
+
+--
 -- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -324,6 +393,14 @@ ALTER TABLE ONLY public.orders
 
 ALTER TABLE ONLY public.products
     ADD CONSTRAINT products_pkey PRIMARY KEY ("productId");
+
+
+--
+-- Name: products products_categoryId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT "products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES public.categories("categoryId");
 
 
 --
