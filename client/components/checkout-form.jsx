75778fba3ff352e310eb7cfaa24ee4d3d485dd71
Sierra Henderson <henderson.sierra@yahoo.com>
2020-08-13 16:30:51 -0700

    Styling for checkout form

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -11,6 +11,7 @@ export default class CheckoutForm extends React.Component {
       expYearValue: '',
       cvvValue: '',
       shippingAddressValue: '',
+      shippingAddress2Value: '',
       cityValue: '',
       stateValue: '',
       zipCodeValue: '',
@@ -33,11 +34,32 @@ export default class CheckoutForm extends React.Component {
 
   handleSubmit(callback) {
     event.preventDefault();
-    if (this.state.agree && this.state.nameValue && this.state.creditCardValue && this.state.shippingAddressValue) {
+    if (
+      this.state.agree &&
+      this.state.firstNameValue &&
+      this.state.lastNameValue &&
+      this.state.creditCardValue &&
+      this.state.expMonthValue &&
+      this.state.expYearValue &&
+      this.state.expMonthValue &&
+      this.state.cvvValue &&
+      this.state.shippingAddressValue &&
+      this.state.cityValue &&
+      this.state.stateValue &&
+      this.state.zipCodeValue
+    ) {
       const obj = {
-        name: this.state.nameValue,
+        firstName: this.state.firstNameValue,
+        lastName: this.state.lastNameValue,
         creditCard: this.state.creditCardValue,
-        shippingAddress: this.state.shippingAddressValue
+        expMonth: this.state.expMonthValue,
+        expYear: this.state.expYearValue,
+        cvv: this.state.cvvValue,
+        shippingAddress: this.state.shippingAddressValue,
+        shippingAddress2: this.state.shippingAddress2Value,
+        city: this.state.cityValue,
+        state: this.state.stateValue,
+        zipCode: this.state.zipCodeValue
       };
       callback(obj);
     } else {
@@ -162,51 +184,74 @@ export default class CheckoutForm extends React.Component {
         <h1 className="font-weight-bold">My Cart</h1>
         <h4 className="text-muted">{`Order Total: $${totalPrice}`}</h4>
         <form onSubmit={() => this.handleSubmit(this.props.placeOrder)}>
-          <div className="form-group">
-            <label htmlFor="first-name">First Name<span className="required">*</span></label>
-            <input type="text" className="form-control" name="first-name" id="first-name" value={this.state.firstNameValue} onChange={() => this.handleInputChange('firstNameValue')} />
-            <p className={requiredFirstNameCheck}>Please submit your first name</p>
-            <label htmlFor="last-name">Last Name<span className="required">*</span></label>
-            <input type="text" className="form-control" name="last-name" id="last-name" value={this.state.lastNameValue} onChange={() => this.handleInputChange('lastNameValue')} />
-            <p className={requiredLastNameCheck}>Please submit your last name</p>
+          <h5 className="mt-4">Shipping Information</h5>
+          <div className="form-group form-row">
+            <div className="col-md">
+              <label htmlFor="first-name">First Name<span className="required">*</span></label>
+              <input type="text" className="form-control" name="first-name" id="first-name" value={this.state.firstNameValue} onChange={() => this.handleInputChange('firstNameValue')} />
+              <p className={requiredFirstNameCheck}>Please submit your first name</p>
+            </div>
+            <div className="col-md">
+              <label htmlFor="last-name">Last Name<span className="required">*</span></label>
+              <input type="text" className="form-control" name="last-name" id="last-name" value={this.state.lastNameValue} onChange={() => this.handleInputChange('lastNameValue')} />
+              <p className={requiredLastNameCheck}>Please submit your last name</p>
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="shippingAddress">Address 1<span className="required">*</span></label>
-            <input className="form-control" name="shippingAddress" id="shippingAddress" value={this.state.shippingAddressValue} onChange={() => this.handleInputChange('shippingAddressValue')} />
-            <p className={requiredShippingAddressCheck}>Please submit your street address</p>
-            <label htmlFor="shippingAddress2">Address 2</label>
-            <input className="form-control" name="shippingAddress2" id="shippingAddress2" value={this.state.shippingAddress2Value} onChange={() => this.handleInputChange('shippingAddress2Value')} />
+          <div className="form-group form-row">
+            <div className="col-md">
+              <label htmlFor="shippingAddress">Address 1<span className="required">*</span></label>
+              <input className="form-control" name="shippingAddress" id="shippingAddress" value={this.state.shippingAddressValue} onChange={() => this.handleInputChange('shippingAddressValue')} />
+              <p className={requiredShippingAddressCheck}>Please submit your street address</p>
+            </div>
+            <div className="col-md">
+              <label htmlFor="shippingAddress2">Address 2</label>
+              <input className="form-control" name="shippingAddress2" id="shippingAddress2" value={this.state.shippingAddress2Value} onChange={() => this.handleInputChange('shippingAddress2Value')} />
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="city">City<span className="required">*</span></label>
-            <input type="text" className="form-control" name="city" id="city" value={this.state.cityValue} onChange={() => this.handleInputChange('cityValue')} />
-            <p className={requiredCityCheck}>Please submit your city</p>
-            <label htmlFor="state">State<span className="required">*</span></label>
-            <input type="text" className="form-control" name="state" id="state" value={this.state.stateValue} onChange={() => this.handleInputChange('stateValue')} />
-            <p className={requiredStateCheck}>Please submit your state</p>
-            <label htmlFor="zipCode">Zip Code<span className="required">*</span></label>
-            <input type="text" className="form-control" name="zipCode" id="zipCode" value={this.state.zipCodeValue} onChange={() => this.handleInputChange('zipCodeValue')} />
-            <p className={requiredZipCodeCheck}>Please submit your zip code</p>
+          <div className="form-group form-row">
+            <div className="col-md">
+              <label htmlFor="city">City<span className="required">*</span></label>
+              <input type="text" className="form-control" name="city" id="city" value={this.state.cityValue} onChange={() => this.handleInputChange('cityValue')} />
+              <p className={requiredCityCheck}>Please submit your city</p>
+            </div>
+            <div className="col-md">
+              <label htmlFor="state">State<span className="required">*</span></label>
+              <input type="text" className="form-control" name="state" id="state" value={this.state.stateValue} onChange={() => this.handleInputChange('stateValue')} />
+              <p className={requiredStateCheck}>Please submit your state</p>
+            </div>
+            <div className="col-md">
+              <label htmlFor="zipCode">Zip Code<span className="required">*</span></label>
+              <input type="text" className="form-control" name="zipCode" id="zipCode" value={this.state.zipCodeValue} onChange={() => this.handleInputChange('zipCodeValue')} />
+              <p className={requiredZipCodeCheck}>Please submit your zip code</p>
+            </div>
           </div>
-          <div className="form-group">
+          <hr />
+          <h5>Payment Information</h5>
+          <div className="form-group form-row">
             <label htmlFor="creditCard">Credit Card<span className="required">*</span></label>
             <input type="text" className="form-control" name="creditCard" id="creditCard" value={this.state.creditCardValue} onChange={() => this.handleInputChange('creditCardValue')} />
             <p className={requiredCreditCardCheck}>Please submit your 16 digit credit card number</p>
           </div>
-          <div className="form-group">
-            <label htmlFor="month">Month<span className="required">*</span></label>
-            <input type="text" className="form-control" name="month" id="month" value={this.state.expMonthValue} onChange={() => this.handleInputChange('expMonthValue')} />
-            <p className={requiredExpMonthCheck}>Please select a month</p>
-            <label htmlFor="year">Year<span className="required">*</span></label>
-            <input type="text" className="form-control" name="year" id="year" value={this.state.expYearValue} onChange={() => this.handleInputChange('expYearValue')} />
-            <p className={requiredExpYearCheck}>Please select a year</p>
-            <label htmlFor="cvv">CVV<span className="required">*</span></label>
-            <input type="text" className="form-control" name="cvv" id="cvv" value={this.state.cvvValue} onChange={() => this.handleInputChange('cvvValue')} />
-            <p className={requiredCvvCheck}>Please submit your CVV</p>
+          <div className="form-group form-row">
+            <div className="col-md">
+              <label htmlFor="month">Month<span className="required">*</span></label>
+              <input type="text" className="form-control" name="month" id="month" value={this.state.expMonthValue} onChange={() => this.handleInputChange('expMonthValue')} />
+              <p className={requiredExpMonthCheck}>Please select a month</p>
+            </div>
+            <div className="col-md">
+              <label htmlFor="year">Year<span className="required">*</span></label>
+              <input type="text" className="form-control" name="year" id="year" value={this.state.expYearValue} onChange={() => this.handleInputChange('expYearValue')} />
+              <p className={requiredExpYearCheck}>Please select a year</p>
+            </div>
+            <div className="col-md">
+              <label htmlFor="cvv">CVV<span className="required">*</span></label>
+              <input type="text" className="form-control" name="cvv" id="cvv" value={this.state.cvvValue} onChange={() => this.handleInputChange('cvvValue')} />
+              <p className={requiredCvvCheck}>Please submit your CVV</p>
+            </div>
           </div>
-          <div className="checkbox d-flex">
-            <input type="checkbox" className="mt-1 mr-2" checked={this.state.agree} onChange={this.handleCheckChange}/>
-            <p>I acknowledge that this is purely for demonstration purposes and no real names, addresses, or credit card information should be used for the purposes of this app</p>
+          <div className="checkbox d-flex form-check">
+            <input type="checkbox" className="form-check-input" id="checkbox" checked={this.state.agree} onChange={this.handleCheckChange}/>
+            <label className="form-check-label" htmlFor="checkbox">I acknowledge that this is purely for demonstration purposes and no real names, addresses, or credit card information should be used for the purposes of this app</label>
           </div>
           {requiredWarningCheck}
           <button type="submit" className="btn asana-button mt-3 mb-4">Place Order</button>
